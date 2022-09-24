78dfa5ee274b5d92ff6ff03564d1cb5ea335a8e1
hridoysutar1 <hridoysutar1@gmail.com>
2022-09-25 00:25:37 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmployeesService } from './services/employees.service';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import{HttpClientModule} from '@angular/common/http';
+import { EmployeelistComponent } from './employeelist/employeelist.component';
+import { NewEmployeeComponent } from './new-employee/new-employee.component';
+import{FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeDeleteComponent } from './employee-delete/employee-delete.component';
+import { HomeComponent } from './home/home.component';
+import { SuccessComponent } from './success/success.component';
+import { LoginComponent } from './login/login.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeelistComponent,
+    NewEmployeeComponent,
+    UpdateEmployeeComponent,
+    EmployeeDetailsComponent,
+    EmployeeDeleteComponent,
+    HomeComponent,
+    SuccessComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
